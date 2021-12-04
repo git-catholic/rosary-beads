4f704b285efba86230e531b9f3ee1e8946fd65fe
gtp-tduncan-samsung <gtp.tduncan@gmail.com>
2021-12-03 22:06:21 -0600

    Add routing support. Fix sound loading.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BuzzFeedbackComponent } from './components/testing/buzz-feedback/buzz-feedback.component';
 import { LayoutsComponent } from './components/testing/layouts/layouts.component';
+import { HolyRosaryHomeComponent } from './rosary-prayers/holy-rosary/holy-rosary-home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'holy-rosary', component: HolyRosaryHomeComponent },
   { path: 'testing/layout', component: LayoutsComponent },
   { path: 'testing/buzz', component: BuzzFeedbackComponent }
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   constructor(private languages: SupportedLanguagesService) { }
 
   ngOnInit(): void {
-    this.languages.checkForRedirect();
+    this.languages.checkForRedirect('holy-rosary');
   }
 
 }
