67384759b1843d8a5c0c1eaef33c01a91905fff8
gtp-tduncan <gtp.tduncan@gmail.com>
2021-12-29 11:30:04 -0600

    Update build scripts using generic versions. Update app version number.

--- docker-run.sh
+++ docker-run.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 echo "pwd: $(pwd)"
 
-LATEST_NAME=nginx-rosary-beads:latest
-ROOT_NAME=nginx-rosary-beads-
+# -----------------------------------------------------------------------
+# Change as needed
 
 HOST_PORT=80
 GUEST_PORT=80
 
 # ====================================================================
-#  STOP - nothing to see below the line!
+#  STOP - Nothing below this point should need to be changed.
 # ====================================================================
 
-ROOT_COUNT=`docker ps -a | grep -c ${ROOT_NAME}[0-9]`
-if [[ $# -ne 0 ]]; then
-  ROOT_COUNT=$((ROOT_COUNT+1))
-fi
+PRJ_NAME=`echo ${PWD##*/}`
 
-INST_NAME=${ROOT_NAME}i${ROOT_COUNT}
-INST_NAME_EXISTS=`docker ps -a | grep -c ${INST_NAME}`
+echo "Project: ${PRJ_NAME}"
 
-MOUNT_VALUE=type=bind,source=$(pwd)/docs,target=/var/opt/project
+# -----------------------------------------------------------------------
+IMAGE_LATEST=nginx-${PRJ_NAME}:latest
+IMAGE_ROOT=nginx-${PRJ_NAME}-
 
-if [[ ${INST_NAME_EXISTS} -eq 0 ]] || [[ $# -ne 0 ]]; then
-  DOCKER_CMD=run
-  DOCKER_OPTS="-d -it --mount ${MOUNT_VALUE} -p ${HOST_PORT}:${GUEST_PORT} --name"
-  USE_LATEST_NAME=$LATEST_NAME
+IMAGE_COUNT=`docker ps -a | grep "${IMAGE_ROOT}[0-9]" | grep -vc "[-]inst$"`
+echo "IMAGE_COUNT: ${IMAGE_COUNT}"
+if [[ ${IMAGE_COUNT} -eq 0 ]]; then
+  echo "Create a Docker image with \"./docker-create-image.sh\" first."
+
 else
-  DOCKER_CMD=start
-  DOCKER_OPTS=
-  USE_LATEST_NAME=
+  IMAGE_NAME=${IMAGE_ROOT}$((IMAGE_COUNT))
+
+  INST_NAME=${IMAGE_NAME}-inst
+  INST_NAME_EXISTS=`docker ps -a | grep -c "${INST_NAME}"`
+
+  echo "IMAGE_NAME:  ${IMAGE_NAME}"
+  echo "INST_NAME:   ${INST_NAME}"
+  echo "INST_EXISTS: ${INST_NAME_EXISTS}"
+
+  MOUNT_VALUE=type=bind,source=$(pwd)/docs,target=/var/opt/project
+
+  if [[ ${INST_NAME_EXISTS} -eq 0 ]] || [[ $# -ne 0 ]]; then
+    DOCKER_CMD=run
+    DOCKER_OPTS="-d -it --mount ${MOUNT_VALUE} -p ${HOST_PORT}:${GUEST_PORT} --name"
+    USE_IMAGE_LATEST=$IMAGE_LATEST
+  else
+    DOCKER_CMD=start
+    DOCKER_OPTS=
+    USE_IMAGE_LATEST=
+  fi
+
+  docker ${DOCKER_CMD} ${DOCKER_OPTS} ${INST_NAME} ${USE_IMAGE_LATEST}
 fi
-
-docker ${DOCKER_CMD} ${DOCKER_OPTS} ${INST_NAME} ${USE_LATEST_NAME}
-
