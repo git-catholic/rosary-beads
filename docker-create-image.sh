d81e8e001b6d4da7880a7ad62190b29e7a16ec5c
gtp-tduncan <gtp.tduncan@gmail.com>
2021-12-29 11:30:04 -0600

    Update build scripts using generic versions

--- docker-create-image.sh
+++ docker-create-image.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-IMAGE_LATEST=nginx-rosary-beads:latest
-IMAGE_ROOT=nginx-rosary-beads-
+# -----------------------------------------------------------------------
+# Change as needed
+
+HOST_PORT=80
+GUEST_PORT=80
+
+# ====================================================================
+#  STOP - Nothing below this point should need to be changed.
+# ====================================================================
+
+PRJ_NAME=`echo ${PWD##*/}`
+
+echo "Project: ${PRJ_NAME}"
+
+# -----------------------------------------------------------------------
+IMAGE_LATEST=nginx-${PRJ_NAME}:latest
+IMAGE_ROOT=nginx-${PRJ_NAME}-
 
 IMAGE_COUNT=`docker ps -a | grep -c ${IMAGE_ROOT}`
 IMAGE_NAME=${IMAGE_ROOT}$((IMAGE_COUNT+1))
-echo ${IMAGE_NAME}
+echo "Creating image \"${IMAGE_NAME}\"."
 
+# -----------------------------------------------------------------------
 echo "Start build"
 docker build -t ${IMAGE_NAME} -t $IMAGE_LATEST .
 echo "End build"
 echo ""
 echo "Start create"
-docker create --name ${IMAGE_NAME} -p 80:80 $IMAGE_LATEST
+docker create --name ${IMAGE_NAME} -p ${HOST_PORT}:${GUEST_PORT} $IMAGE_LATEST
 echo "End create"
 echo ""
