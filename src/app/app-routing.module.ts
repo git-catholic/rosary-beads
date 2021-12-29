af022ac6de0a5dd8a79d0a189d54fd78dd362abf
gtp-tduncan <gtp.tduncan@gmail.com>
2021-12-29 11:48:34 -0600

    Update to use hash routing strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BuzzFeedbackComponent } from './components/testing/buzz-feedback/buzz-feedback.component';
 import { LayoutsComponent } from './components/testing/layouts/layouts.component';
+import { HolyRosaryHomeComponent } from './rosary-prayers/holy-rosary/holy-rosary-home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'testing/layout', component: LayoutsComponent },
-  { path: 'testing/buzz', component: BuzzFeedbackComponent }
+  { path: 'testing/buzz', component: BuzzFeedbackComponent },
+  { path: 'holy-rosary', component: HolyRosaryHomeComponent },
+  { path: '', redirectTo: 'holy-rosary', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
