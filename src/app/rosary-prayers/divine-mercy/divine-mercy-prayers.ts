28fd1d3f7f29ab0e531c5b33e3e724c7b19a2ddd
gtp-tduncan <gtp.tduncan@gmail.com>
2022-01-17 09:21:30 -0600

    Add initial Divine Mercy prayers

---
+++ src/app/rosary-prayers/divine-mercy/divine-mercy-prayers.ts
@@ -0,0 +1,96 @@
+import { GroupPrayer } from "src/app/prayers/common-prayers";
+
+export class DivineMercyOptionalOpening extends GroupPrayer {
+
+  get all(): string {
+    return $localize`:@@divineMercyOptionalOpening:You expired, Jesus, but the source of life gushed forth for souls,
+      and the ocean of mercy opened up for the whole world.
+      O Fount of Life, unfathomable Divine Mercy,
+      envelop the whole world and empty Yourself out upon us.`;
+  }
+
+  get name(): string {
+    return $localize`:@@divineMercyOptionalOpeningLabel1:Divine Mercy Opening #1`;
+  }
+  
+}
+
+/* ================================================================================ */
+
+export class DivineMercyBloodAndWater extends GroupPrayer {
+
+  get all(): string {
+    // TODO - update
+    return $localize`:@@divineMercyBloodAndWater:O Blood and Water, which gushed forth from the Heart of Jesus as a fountain of Mercy for us, I trust in You!`
+  }
+
+  get name(): string {
+    // TODO - what is the real name of this prayer?
+    return $localize`:@@divineMercyBloodAndWaterLabel:Divine Mercy Blood and Water`;
+  }
+  
+}
+
+/* ================================================================================ */
+
+export class DivineMercyEternalFather extends GroupPrayer {
+
+  get all(): string {
+    // TODO - update
+    return $localize`:@@divineMercyEternalFather:Eternal Father, I offer you the Body and Blood, Soul and Divinity of Your Dearly Beloved Son, Our Lord, Jesus Christ, in atonement for our sins and those of the whole world.`
+  }
+
+  get name(): string {
+    // TODO - what is the real name of this prayer?
+    return $localize`:@@divineMercyEternalFatherLabel:Divine Mercy Eternal Father`;
+  }
+
+}
+
+/* ================================================================================ */
+
+export class DivineMercyHaveMercyOnUs extends GroupPrayer {
+
+  get all(): string {
+    // TODO - update
+    return $localize`:@@divineMercyHaveMercyOnUs:For the sake of His sorrowful Passion, have mercy on us and on the whole world.`
+  }
+
+  get name(): string {
+    // TODO - what is the real name of this prayer?
+    return $localize`:@@divineMercyHaveMercyOnUsLabel:Divine Mercy Have Mercy on Us`;
+  }
+
+}
+
+/* ================================================================================ */
+
+export class DivineMercyHolyMightyOne extends GroupPrayer {
+
+  get all(): string {
+    // TODO - update
+    return $localize`:@@divineMercyHolyMightyOne:Holy God, Holy Mighty One, Holy Immortal One, have mercy on us and on the whole world.`
+  }
+
+  get name(): string {
+    // TODO - what is the real name of this prayer?
+    return $localize`:@@divineMercyHolyMightyOneLabel:Divine Mercy Holy Mighty One`;
+  }
+
+}
+
+/* ================================================================================ */
+
+export class DivineMercyOptionalClosing extends GroupPrayer {
+
+  get all(): string {
+    // TODO - update
+    return $localize`:@@divineMercyOptionalClosing:Eternal God, in whom mercy is endless and the treasury of compassion inexhaustible, look kindly upon us and increase Your mercy in us, that in difficult moments we might not despair nor become despondent, but with great confidence submit ourselves to Your holy will, which is Love and Mercy itself.`
+  }
+
+  get name(): string {
+    // TODO - what is the real name of this prayer?
+    return $localize`:@@divineMercyOptionalClosingLabel:Divine Mercy Holy Mighty One`;
+  }
+
+}
