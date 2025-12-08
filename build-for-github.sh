3bd4b570d52a7bed8cdbf034903c6d67df76ff5a
git-catholic <git-catholic@users.noreply.github.com>
2025-12-08 08:42:29 -0600

    Update build script for Angular 19

--- build-for-github.sh
+++ build-for-github.sh
@@ -25,7 +25,7 @@ else
   npm version prerelease
   ng build --output-path docs --base-href "#47;${PRJ_NAME}/"
 
-  cp -p src/redirect-index.html docs/index.html
+  cp -p src/redirect-index.html docs/browser/index.html
   
   COMMIT_VERSION=`npm version | grep -i "${PRJ_NAME}" | awk -F"[:]" '{ print $1,$2 }' | sed -e "s/',//g"`
   CV_CLEAN=`echo ${COMMIT_VERSION}`
